39a2548d1e3dfc47dedd3307dbe868ce18c1f38d
Kartik Hatwar <kartikhatwar98@gmail.com>
2022-12-13 20:08:27 +0530

    remove pwa

--- scr/views/js/index.js
+++ scr/views/js/index.js
@@ -1,7 +1,7 @@
 
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('../sw.js');
-}
-else{
-    console.log('You are offline');
-}
+// if('serviceWorker' in navigator){
+//     navigator.serviceWorker.register('../sw.js');
+// }
+// else{
+//     console.log('You are offline');
+// }
