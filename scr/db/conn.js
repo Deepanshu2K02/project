ff01efa49627e2e97265a9a61d669abc94826719
Kartik Hatwar <kartikhatwar98@gmail.com>
2022-12-16 14:52:34 +0530

    Database connected

--- scr/db/conn.js
+++ scr/db/conn.js
@@ -1,11 +1,15 @@
 import { createRequire } from "module";
+import envlocal from "dotenv";
+
+envlocal.config();
 const require = createRequire(import.meta.url);
 
 export const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 //Set up default mongoose connection
-const mongoDB = 'mongodb://localhost:27017/Text-Tools';
-mongoose.connect(mongoDB, { useNewUrlParser: true }).then(()=>{
+
+const DB = process.env.DATABASE;
+mongoose.connect(DB, { useNewUrlParser: true }).then(()=>{
     // console.log('Connected');
 })
 
