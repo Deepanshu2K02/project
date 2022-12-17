b37110c1517ad4aa8abb3bdee408066582673196
Kartik Hatwar <kartikhatwar98@gmail.com>
2022-12-17 15:10:20 +0530

    debug#8

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -12,8 +12,8 @@ export const Uauth = async (req,res,next)=>{
     
     if(!token) res.redirect('/loginpage');
     else{
-        // res.send(req.signedCookies);
-        const verifyUser = jwt.verify(token,"Text-Tools_secret_key_CreatedBy_Kartik_Hatwar_for_registered_users");
+        res.send(req.signedCookies.token);
+        // const verifyUser = jwt.verify(token,"Text-Tools_secret_key_CreatedBy_Kartik_Hatwar_for_registered_users");
      
         if(verifyUser) res.send(verifyUser);
         // if(verifyUser.email) next();
