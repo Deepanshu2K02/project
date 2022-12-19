017bb825ecead0bc937531e4cb6e69e4021b94d2
Kartik Hatwar <kartikhatwar98@gmail.com>
2022-12-19 14:24:26 +0530

    service worker reduced

--- src/views/service_worker.js
+++ src/views/service_worker.js
@@ -5,42 +5,42 @@ const assets = [
 ]
 
 self.addEventListener('install', function(event) {
-    console.log('used to register the service worker')
-      event.waitUntil(
-      caches.open(static_cache)
-      .then(function(cache) {
-        cache.addAll(assets)
-      })
-      .then(self.skipWaiting())
-      )
+      self.skipWaiting();
+      // event.waitUntil(
+      // caches.open(static_cache)
+      // .then(function(cache) {
+      //   cache.addAll(assets)
+      // })
+      // .then(self.skipWaiting())
+      // )
   })
   
   self.addEventListener('fetch', function(event) {
 
-      event.respondWith(
-      fetch(event.request)
-        .catch(() => {
-          console.log('fetching resource')
-          return caches.open(static_cache)
-            .then((cache) => {
-              return cache.match(event.request)
-            })
-        })
-    )
+    //   event.respondWith(
+    //   fetch(event.request)
+    //     .catch(() => {
+    //       console.log('fetching resource')
+    //       return caches.open(static_cache)
+    //         .then((cache) => {
+    //           return cache.match(event.request)
+    //         })
+    //     })
+    // )
   })
   
   self.addEventListener('activate', function(event) {
-      event.waitUntil(
-          caches.keys()
-            .then((keyList) => {
-              return Promise.all(keyList.map((key) => {
-                if (key !== static_cache) {
-                  return caches.delete(key)
-                }
-              }))
-            })
-            .then(() => self.clients.claim())
-        )
+      // event.waitUntil(
+      //     caches.keys()
+      //       .then((keyList) => {
+      //         return Promise.all(keyList.map((key) => {
+      //           if (key !== static_cache) {
+      //             return caches.delete(key)
+      //           }
+      //         }))
+      //       })
+      //       .then(() => self.clients.claim())
+      //   )
   })
 
 
